d41a9c4f9580cda80dd28d915e6db21484f6ce4e
per <per@alipsa.se>
2025-12-10 21:44:27 +0100

    release 0.1.0

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [[ $(git status --porcelain) ]]; then
+  echo "Git changes detected, commit all changes first before releasing"
+  exit
+fi
+mvn -Prelease clean site deploy || exit 1
+echo "Release successful!"
--- resume.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-codex resume 019b043f-9031-7761-aefd-68d192973bca
